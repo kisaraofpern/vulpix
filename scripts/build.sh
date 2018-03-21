a51a22c9f4582910b77bdd958009535ae9d592d2
Elizabeth Chhouk <echhouk@noteflight.com>
2018-03-21 13:24:40 -0400

    Use zip because the Docker file has zip now

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,4 @@
 echo "Archive the app..."
-tar -czvf vulpix.tgz ./* --exclude .git --exclude .gitignore --exclude .DS_Store
-aws s3 cp vulpix.tgz s3://kisaras-playground
-rm vulpix.tgz
+zip -czvf vulpix.zip ./* --exclude .git --exclude .gitignore --exclude .DS_Store
+aws s3 cp vulpix.zip s3://kisaras-playground
+rm vulpix.zip
